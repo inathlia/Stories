ec823a1df83d9ac1795cb61ec884822ee8c5d7a8
Nathalia Tenaglia <nathtenaglia@hotmail.com>
2024-01-09 11:06:18 -0300

    feat: slide class and show method

---
+++ dist/Slide.js
@@ -0,0 +1,27 @@
+export default class Slide {
+    container;
+    slides;
+    controls;
+    time;
+    index;
+    slide;
+    constructor(container, slides, controls, time = 5000) {
+        this.container = container;
+        this.slides = slides;
+        this.controls = controls;
+        this.time = time;
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.show(this.index);
+    }
+    hide(el) {
+        el.classList.remove("active");
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((el) => this.hide(el));
+        this.slide.classList.add("active");
+    }
+}
+//# sourceMappingURL=Slide.js.map
